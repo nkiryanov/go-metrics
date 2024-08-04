7258cd3f4da61c8c633b225c02a7a773087bb511
Nikolay Kiryanov <nikolay@kiryanov.ru>
2024-08-04 04:47:47 +0300

    Run integration tests with build tag only

--- Makefile
+++ Makefile
@@ -3,6 +3,15 @@ tests: test
 test:
 	go test -race -timeout=60s -count 1 -v ./...
 
+.PHONY: test_integration tests_integration
+tests_integration: test_integration
+test_integration: test_integration
+	go test -race -timeout=60s -count 1 -v ./... -tags=integration
+
+.PHONY: test_all tests_all
+tests_all: test_all
+test_all: test test_integration
+
 .PHONY: fmt
 fmt:
 	go fmt ./...
--- internal/agent/publisher/publisher_test.go
+++ internal/agent/publisher/publisher_test.go
@@ -2,8 +2,6 @@ package publisher
 
 import (
 	"context"
-	"net/http"
-	"sync"
 	"testing"
 	"time"
 
@@ -12,46 +10,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const (
-	halfSecond = time.Millisecond * 500
-)
-
-type capturedReq struct {
-	method string
-	path   string
-}
-
-type testHTTPReceiver struct {
-	requests []capturedReq
-	lock     sync.Mutex
-}
-
-func newTestHTTPReceiver() testHTTPReceiver {
-	return testHTTPReceiver{requests: make([]capturedReq, 0)}
-}
-
-func (rcv *testHTTPReceiver) Handler(w http.ResponseWriter, req *http.Request) {
-	rcv.lock.Lock()
-	rcv.requests = append(rcv.requests, capturedReq{req.Method, req.URL.Path})
-	rcv.lock.Unlock()
-}
-
-func (rcv *testHTTPReceiver) run(listenAddr string, ctx context.Context) {
-	// Run http receiver in background
-	srv := &http.Server{
-		Addr:    listenAddr,
-		Handler: http.HandlerFunc(rcv.Handler),
-	}
-
-	go func() {
-		srv.ListenAndServe() // nolint:errcheck
-	}()
-
-	go func() {
-		<-ctx.Done()
-		srv.Close()
-	}()
-}
+var thirdSecond = time.Millisecond * 300
 
 func TestPublisher_NewHTTPPublisherValidAddr(t *testing.T) {
 	pubAddr := "http://example.com:8080"
@@ -75,45 +34,8 @@ func TestPublisher_NewHTTPPublisherInvalidAddr(t *testing.T) {
 	assert.Equal(t, "publisher: Invalid publisher address", err.Error())
 }
 
-func TestHTTPPublisher_postMetric(t *testing.T) {
-	publisher, _ := NewHTTPPublisher("http://localhost:51493", time.Millisecond, storage.NewMemStorage())
-	// Run test http receiver
-	ctx, stopRcv := context.WithTimeout(context.Background(), halfSecond)
-	rcv := newTestHTTPReceiver()
-	rcv.run("localhost:51493", ctx)
-
-	status, err := publisher.postMetric(storage.CounterTypeName, "counter-stat")
-
-	stopRcv()
-	require.NoError(t, err)
-	require.Equal(t, http.StatusOK, status)
-	require.Len(t, rcv.requests, 1)
-	assert.Equal(t, "POST", rcv.requests[0].method)
-	assert.Equal(t, "/update/counter/counter-stat/0", rcv.requests[0].path)
-}
-
-func TestHTTPPublisher_batchPublish(t *testing.T) {
-	// Prepare storage with some metrics
-	s := storage.NewMemStorage()
-	s.UpdateCounter("counter-stat", 1)
-	s.SetGauge("gauge-stat", 2.01)
-	// Prepare publisher
-	publisher, _ := NewHTTPPublisher("http://localhost:51493", time.Second, s)
-	// Run test http receiver
-	ctx, stopRcv := context.WithTimeout(context.Background(), time.Second)
-	rcv := newTestHTTPReceiver()
-	rcv.run("localhost:51493", ctx)
-
-	publisher.batchPublish()
-
-	stopRcv()
-	require.Len(t, rcv.requests, 2)
-	assert.Contains(t, rcv.requests, capturedReq{"POST", "/update/counter/counter-stat/1"})
-	assert.Contains(t, rcv.requests, capturedReq{"POST", "/update/gauge/gauge-stat/2.01"})
-}
-
 func TestHTTPPublisher_RunStopWithSignal(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), halfSecond)
+	ctx, cancel := context.WithTimeout(context.Background(), thirdSecond)
 	defer cancel()
 	publisher, _ := NewHTTPPublisher("http://example.com", time.Second, storage.NewMemStorage())
 
