87bc6335cc97f4a36ac95cf520fd09b6d1487937
Nikolay Kiryanov <nikolay@kiryanov.ru>
2025-06-09 00:14:42 +0300

    wip

---
+++ internal/storage/pgstorage/storage_test.go
@@ -0,0 +1,37 @@
+package pgstorage
+
+import (
+	"context"
+	"log"
+	"testing"
+
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+)
+
+var testStorage *PgStorage
+
+func TestMain(m *testing.M) {
+	ctx := context.Background()
+
+	// Start PostgreSQL container
+	pgContainer, err := postgres.Run(ctx,
+		"postgres:15-alpine",
+		postgres.WithDatabase("testdb"),
+		postgres.WithUsername("testuser"),
+		postgres.WithPassword("testpass"),
+		postgres.BasicWaitStrategies(),
+	)
+
+	defer func() {
+		if err := testcontainers.TerminateContainer(pgContainer); err != nil {
+			log.Printf("failed to terminate container: %s", err)
+		}
+	}()
+	if err != nil {
+		log.Printf("failed to start container: %s", err)
+		return
+	}
+
+	m.Run()
+}
