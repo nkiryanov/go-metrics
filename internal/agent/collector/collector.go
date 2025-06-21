2ef6974cc952b9f38219e5fcb83d8169a41f6b5b
Nikolay Kiryanov <nikolay@kiryanov.ru>
2025-06-21 20:24:30 +0300

    Agent: rewrite `Capturer` as `Collector` with some new logic

--- internal/agent/collector/collector.go
+++ internal/agent/collector/collector.go
@@ -1,15 +1,45 @@
 package collector
 
 import (
+	"time"
+
 	"context"
 	"github.com/nkiryanov/go-metrics/internal/models"
 )
 
 // Collector collects system metrics at configured intervals, stores them internally
 type Collector interface {
-	// Run starts the collector with its configured interval
-	Run(ctx context.Context) error
+	// Collect metrics. Should store them internally
+	Collect(ctx context.Context) error
 
 	// List collected metrics. Should be fast enough
 	List(ctx context.Context) ([]models.Metric, error)
+
 }
+
+
+// Run Collector periodically by 'interval'
+func Run(ctx context.Context, c Collector, interval time.Duration) error {
+	var err error
+
+	err = c.Collect(ctx)  // collect metrics at start
+	if err != nil {
+		return err
+	}
+
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-ticker.C:
+			err = c.Collect(ctx)
+			if err != nil {
+				return err
+			}
+		}
+	}
+}
+
